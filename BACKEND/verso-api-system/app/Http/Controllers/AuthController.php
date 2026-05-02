1fb0c0e160d3c3e355fe1af1414d830650c5bf3e
TheRealNightmare <nnahid929@gmail.com>
2026-05-03 00:32:22 +0600

    css fixes and cleanups

--- BACKEND/verso-api-system/app/Http/Controllers/AuthController.php
+++ BACKEND/verso-api-system/app/Http/Controllers/AuthController.php
@@ -6,7 +6,6 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
