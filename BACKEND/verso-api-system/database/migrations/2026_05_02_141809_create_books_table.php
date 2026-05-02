1bfef7a5cda0ab63fb67ace7e598f1c92705fcaf
TheRealNightmare <nnahid929@gmail.com>
2026-05-02 20:53:48 +0600

    database migration files for book, book_contents, histories, bookmark, favourite, reviews

---
+++ BACKEND/verso-api-system/database/migrations/2026_05_02_141809_create_books_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('author');
+            $table->text('description')->nullable();
+            $table->string('cover_image_url')->nullable();
+            $table->string('genre')->nullable();
+            $table->decimal('average_rating', 3, 1)->default(0);
+            $table->integer('published_year')->nullable();
+            $table->integer('gutenberg_id')->unique()->nullable();
+            $table->boolean('is_exclusive')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('books');
+    }
+};
