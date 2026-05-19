377c4ad8f000632f14c3eca8781363fa32fa7258
miraz <nnahid929@gmail.com>
2026-05-19 16:42:16 +0600

    added chapter to bookcontent model

--- BACKEND/verso-api-system/app/Models/BookContent.php
+++ BACKEND/verso-api-system/app/Models/BookContent.php
@@ -6,7 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class BookContent extends Model
 {
-    protected $fillable = ['book_id', 'content'];
+    protected $fillable = ['book_id', 'content', 'chapters'];
+
+    protected $casts = [
+        'chapters' => 'array',
+    ];
 
     public function book()
     {
