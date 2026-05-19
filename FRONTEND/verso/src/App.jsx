62a6c8dd8cd037ad08bdeb026b2cc2124d5d8fac
fairoze <fairozef24@gmail.com>
2026-05-19 13:44:06 +0600

    footer full width now fixes

--- FRONTEND/verso/src/App.jsx
+++ FRONTEND/verso/src/App.jsx
@@ -44,13 +44,15 @@ function AppContent() {
   }
 
   return (
-    <div className="flex min-h-screen bg-[#f8f6f2]">
-      <Sidebar />
-      <main className="flex-1 flex flex-col min-w-0">
-        {!isReadingMode && <TopBar />}
-        <div className="flex-1 p-6">{appRoutes}</div>
-        {!isReadingMode && <Footer />}
-      </main>
+    <div className="flex flex-col min-h-screen bg-[#f8f6f2]">
+      <div className="flex flex-1 min-h-0">
+        <Sidebar />
+        <main className="flex-1 flex flex-col min-w-0">
+          {!isReadingMode && <TopBar />}
+          <div className="flex-1 p-6">{appRoutes}</div>
+        </main>
+      </div>
+      {!isReadingMode && <Footer />}
     </div>
   );
 }
